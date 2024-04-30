823cb611142fad11dd644e0d53201ad82e9c94eb
Gabriel Scherer <gabriel.scherer@gmail.com>
2024-04-30 14:55:18 +0200

    Merge pull request PR#13121 from gasche/matching-partiality-testsuite
    The Pattern-Matching Bug: testsuite improvements
    
    (cherry picked from commit 63ec086cef237c23aa0120eadc80dfaf2ab7f89c)

---
+++ testsuite/tests/match-side-effects/check_partial.ml
@@ -0,0 +1,134 @@
+(* TEST
+ flags = "-dlambda";
+ expect;
+*)
+
+(* This test exercises pattern-matching examples that mix mutable
+   state with code execution (through guards or lazy patterns). Some
+   of those tests appear to be exhaustive to the type-checker but are
+   in fact not exhaustive, forcing the pattern-matching compiler to
+   add Match_failure clauses for soundness. The pattern-matching
+   compiler also sometimes conservatively add Match_failure clauses in
+   cases that were in fact exhaustive.
+*)
+
+type _ t =
+  | Int : int -> int t
+  | True : bool t
+  | False : bool t
+
+let lazy_total : _ * bool t -> int = function
+  | ({ contents = _ }, True) -> 0
+  | ({ contents = lazy () }, False) -> 12
+(* This pattern-matching is in fact total: a Match_failure case is
+   not necessary for soundness. *)
+[%%expect {|
+0
+type _ t = Int : int -> int t | True : bool t | False : bool t
+(let
+  (lazy_total/289 =
+     (function {nlocal = 0}
+       param/291[value<
+                  (consts ())
+                   (non_consts ([0: *,
+                                 value<
+                                  (consts (1 0))
+                                   (non_consts ([0: value<int>]))>]))>]
+       : int
+       (let
+         (*match*/293 =o? (field_mut 0 (field_imm 0 param/291))
+          *match*/294 =a? (field_imm 1 param/291))
+         (if (isint *match*/294)
+           (if *match*/294
+             (let
+               (*match*/301 =?
+                  (let (tag/296 =a[value<int>] (caml_obj_tag *match*/293))
+                    (if (%int_equal tag/296 250) (field_mut 0 *match*/293)
+                      (if
+                        (|| (%int_equal tag/296 246)
+                          (%int_equal tag/296 244))
+                        (apply (field_imm 1 (global CamlinternalLazy!))
+                          (opaque *match*/293) never_inline)
+                        *match*/293))))
+               12)
+             0)
+           (raise (makeblock 0 (getpredef Match_failure/49!!) [0: "" 6 37]))))))
+  (apply (field_imm 1 (global Toploop!)) "lazy_total" lazy_total/289))
+val lazy_total : unit lazy_t ref * bool t -> int = <fun>
+|}];;
+
+let lazy_needs_partial : _ * bool t ref -> int = function
+  | (_, { contents = True }) -> 0
+  | (lazy (), { contents = False }) -> 12
+(* This pattern-matching is partial: a Match_failure case is
+   necessary for soundness. *)
+[%%expect {|
+(let
+  (lazy_needs_partial/303 =
+     (function {nlocal = 0}
+       param/305[value<(consts ()) (non_consts ([0: *, *]))>] : int
+       (catch
+         (let
+           (*match*/306 =a? (field_imm 0 param/305)
+            *match*/308 =o? (field_mut 0 (field_imm 1 param/305)))
+           (if (isint *match*/308)
+             (if *match*/308
+               (let
+                 (*match*/311 =?
+                    (let (tag/310 =a[value<int>] (caml_obj_tag *match*/306))
+                      (if (%int_equal tag/310 250) (field_mut 0 *match*/306)
+                        (if
+                          (|| (%int_equal tag/310 246)
+                            (%int_equal tag/310 244))
+                          (apply (field_imm 1 (global CamlinternalLazy!))
+                            (opaque *match*/306) never_inline)
+                          *match*/306)))
+                  *match*/313 =o? (field_mut 0 (field_imm 1 param/305)))
+                 (if (isint *match*/313) (if *match*/313 12 (exit 3))
+                   (exit 3)))
+               0)
+             (exit 3)))
+        with (3)
+         (raise (makeblock 0 (getpredef Match_failure/49!!) [0: "" 1 49])))))
+  (apply (field_imm 1 (global Toploop!)) "lazy_needs_partial"
+    lazy_needs_partial/303))
+val lazy_needs_partial : unit lazy_t * bool t ref -> int = <fun>
+|}];;
+
+let guard_total : bool t ref -> int = function
+  | _ when Sys.opaque_identity false -> 1
+  | { contents = True } -> 0
+  | { contents = False } -> 12
+(* This pattern-matching is total: a Match_failure case is not
+   necessary for soundness. *)
+[%%expect {|
+(let
+  (guard_total/314 =
+     (function {nlocal = 0} param/408 : int
+       (if (opaque 0) 1
+         (let (*match*/409 =o? (field_mut 0 param/408))
+           (switch* *match*/409 case int 0: 0
+                                case int 1: 12)))))
+  (apply (field_imm 1 (global Toploop!)) "guard_total" guard_total/314))
+val guard_total : bool t ref -> int = <fun>
+|}];;
+
+let guard_needs_partial : bool t ref -> int = function
+  | { contents = True } -> 0
+  | _ when Sys.opaque_identity false -> 1
+  | { contents = False } -> 12
+(* This pattern-matching is partial: a Match_failure case is
+   necessary for soundness.
+
+   FAIL: the compiler is currently unsound here. *)
+[%%expect {|
+(let
+  (guard_needs_partial/410 =
+     (function {nlocal = 0} param/412 : int
+       (let (*match*/413 =o? (field_mut 0 param/412))
+         (catch (if (isint *match*/413) (if *match*/413 (exit 9) 0) (exit 9))
+          with (9) (if (opaque 0) 1 12)))))
+  (apply (field_imm 1 (global Toploop!)) "guard_needs_partial"
+    guard_needs_partial/410))
+val guard_needs_partial : bool t ref -> int = <fun>
+|}];;
--- testsuite/tests/match-side-effects/partiality.ml
+++ testsuite/tests/match-side-effects/partiality.ml
@@ -45,24 +45,45 @@ val f : t -> int = <fun>
    inside a mutable position. *)
 type t = {a: bool; mutable b: int option}
 
-let f x =
+let simple x =
   match x with
-  | {a = false; b = _} -> 0
-  | {a = _;     b = None} -> 1
-  | {a = true;  b = Some y} -> y
+  | {b = None} -> 1
+  | {b = Some y} -> y
 ;;
 (* Performance expectation: there should not be a Match_failure case. *)
 [%%expect {|
 0
 type t = { a : bool; mutable b : int option; }
 (let
-  (f/303 =
-     (function {nlocal = 0} x/304 : int
-       (if (field_int 0 x/304)
-         (let (*match*/308 =o? (field_mut 1 x/304))
-           (if *match*/308 (field_imm 0 *match*/308) 1))
+  (simple/303 =
+     (function {nlocal = 0} x/305 : int
+       (let (*match*/308 =o? (field_mut 1 x/305))
+         (if *match*/308 (field_imm 0 *match*/308) 1))))
+  (apply (field_imm 1 (global Toploop!)) "simple" simple/303))
+val simple : t -> int = <fun>
+|}]
+
+(* This more complex case has the switch on [b] split across two cases
+   on [a], so it may need a [Match_failure] for soundness -- it does
+   if the two accesses to [b] are done on different reads of the same
+   mutable field.
+
+   PASS: a single read of [field_mut 1 x], no Match_failure case. *)
+let f x =
+  match x with
+  | {a = false; b = _} -> 0
+  | {a = _;     b = None} -> 1
+  | {a = true;  b = Some y} -> y
+;;
+[%%expect {|
+(let
+  (f/309 =
+     (function {nlocal = 0} x/310 : int
+       (if (field_int 0 x/310)
+         (let (*match*/314 =o? (field_mut 1 x/310))
+           (if *match*/314 (field_imm 0 *match*/314) 1))
          0)))
-  (apply (field_imm 1 (global Toploop!)) "f" f/303))
+  (apply (field_imm 1 (global Toploop!)) "f" f/309))
 val f : t -> int = <fun>
 |}]
 
@@ -77,7 +98,7 @@ let f r =
   | None -> 3
 ;;
 (* Correctness condition: there should either be a single
-   (field_mut 1) access, or the second access should include
+   (field_mut 0) access, or the second access should include
    a Match_failure case.
 
    FAIL: the second occurrence of (field_mut 0) is used with a direct
@@ -85,25 +106,25 @@ let f r =
    unsound here. *)
 [%%expect {|
 (let
-  (f/310 =
-     (function {nlocal = 0} r/311 : int
+  (f/316 =
+     (function {nlocal = 0} r/317 : int
        (region
          (let
-           (*match*/313 =[value<(consts (0)) (non_consts ([0: ?]))>]
-              (makelocalblock 0 (*) r/311))
+           (*match*/319 =[value<(consts (0)) (non_consts ([0: ?]))>]
+              (makelocalblock 0 (*) r/317))
            (catch
-             (if *match*/313
-               (let (*match*/315 =o? (field_mut 0 (field_imm 0 *match*/313)))
-                 (if *match*/315 (exit 11) 0))
-               (exit 11))
-            with (11)
-             (if (seq (setfield_ptr 0 r/311 0) 0) 1
-               (if *match*/313
+             (if *match*/319
+               (let (*match*/321 =o? (field_mut 0 (field_imm 0 *match*/319)))
+                 (if *match*/321 (exit 13) 0))
+               (exit 13))
+            with (13)
+             (if (seq (setfield_ptr 0 r/317 0) 0) 1
+               (if *match*/319
                  (let
-                   (*match*/317 =o? (field_mut 0 (field_imm 0 *match*/313)))
-                   (field_imm 0 *match*/317))
+                   (*match*/323 =o? (field_mut 0 (field_imm 0 *match*/319)))
+                   (field_imm 0 *match*/323))
                  3)))))))
-  (apply (field_imm 1 (global Toploop!)) "f" f/310))
+  (apply (field_imm 1 (global Toploop!)) "f" f/316))
 val f : int option ref -> int = <fun>
 |}]
 
@@ -123,11 +144,11 @@ let test = function
 0
 type _ t = Int : int -> int t | Bool : bool -> bool t
 (let
-  (test/321 =
+  (test/327 =
      (function {nlocal = 0}
-       param/324[value<(consts (0)) (non_consts ([0: ?]))>] : int
-       (if param/324 (field_imm 0 (field_imm 0 param/324)) 0)))
-  (apply (field_imm 1 (global Toploop!)) "test" test/321))
+       param/330[value<(consts (0)) (non_consts ([0: ?]))>] : int
+       (if param/330 (field_imm 0 (field_imm 0 param/330)) 0)))
+  (apply (field_imm 1 (global Toploop!)) "test" test/327))
 val test : int t option -> int = <fun>
 |}]
 
@@ -145,11 +166,11 @@ let test = function
 0
 type _ t = Int : int -> int t | Bool : bool -> bool t
 (let
-  (test/329 =
-     (function {nlocal = 0} param/331 : int
-       (let (*match*/332 =o? (field_mut 0 param/331))
-         (if *match*/332 (field_imm 0 (field_imm 0 *match*/332)) 0))))
-  (apply (field_imm 1 (global Toploop!)) "test" test/329))
+  (test/335 =
+     (function {nlocal = 0} param/337 : int
+       (let (*match*/338 =o? (field_mut 0 param/337))
+         (if *match*/338 (field_imm 0 (field_imm 0 *match*/338)) 0))))
+  (apply (field_imm 1 (global Toploop!)) "test" test/335))
 val test : int t option ref -> int = <fun>
 |}]
 
@@ -170,11 +191,11 @@ let test n =
 0
 type _ t = Int : int -> int t | Bool : bool -> bool t
 (let
-  (test/337 =
-     (function {nlocal = 0} n/338? : int
+  (test/343 =
+     (function {nlocal = 0} n/344? : int
        (region
          (let
-           (*match*/341 =[value<(consts (0)) (non_consts ([0: ?]))>]
+           (*match*/347 =[value<(consts (0)) (non_consts ([0: ?]))>]
               (makelocalblock 0 (value<
                                   (consts ())
                                    (non_consts ([0: *,
@@ -188,13 +209,57 @@ type _ t = Int : int -> int t | Bool : bool -> bool t
                                        (non_consts ([1: value<int>]
                                        [0: value<int>]))>)
                   (makelocalmutable 0 (value<int>) 1) [0: 42])))
-           (if *match*/341
+           (if *match*/347
              (let
-               (*match*/342 =a? (field_imm 0 *match*/341)
-                *match*/344 =o? (field_mut 0 (field_imm 0 *match*/342)))
-               (if *match*/344 (field_imm 0 (field_imm 1 *match*/342))
-                 (%int_neg (field_imm 0 (field_imm 1 *match*/342)))))
+               (*match*/348 =a? (field_imm 0 *match*/347)
+                *match*/350 =o? (field_mut 0 (field_imm 0 *match*/348)))
+               (if *match*/350 (field_imm 0 (field_imm 1 *match*/348))
+                 (%int_neg (field_imm 0 (field_imm 1 *match*/348)))))
              3)))))
-  (apply (field_imm 1 (global Toploop!)) "test" test/337))
+  (apply (field_imm 1 (global Toploop!)) "test" test/343))
 val test : 'a -> int = <fun>
+|}]
+
+
+
+(* In this example, the constructor on which unsound assumptions could
+   be made is not located directly below a mutable constructor, but
+   one level deeper inside an immutable pair constructor (below the
+   mutable constructor). This checks that there is a form of
+   "transitive" propagation of mutability.
+
+   Correctness condition: either there is a single mutable field read,
+   or the accesses below the second mutable read have a Match_failure
+   case.
+*)
+let deep r =
+  match Some r with
+  | Some { contents = ((), None) } -> 0
+  | _ when (r := ((), None); false) -> 1
+  | Some { contents = ((), Some n) } -> n
+  | None -> 3
+;;
+(* FAIL: two different reads (field_mut 0), but no Match_failure case. *)
+[%%expect {|
+(let
+  (deep/353 =
+     (function {nlocal = 0} r/355 : int
+       (region
+         (let
+           (*match*/357 =[value<(consts (0)) (non_consts ([0: ?]))>]
+              (makelocalblock 0 (*) r/355))
+           (catch
+             (if *match*/357
+               (let (*match*/359 =o? (field_mut 0 (field_imm 0 *match*/357)))
+                 (if (field_imm 1 *match*/359) (exit 21) 0))
+               (exit 21))
+            with (21)
+             (if (seq (setfield_ptr 0 r/355 [0: 0 0]) 0) 1
+               (if *match*/357
+                 (let
+                   (*match*/363 =o? (field_mut 0 (field_imm 0 *match*/357)))
+                   (field_imm 0 (field_imm 1 *match*/363)))
+                 3)))))))
+  (apply (field_imm 1 (global Toploop!)) "deep" deep/353))
+val deep : (unit * int option) ref -> int = <fun>
 |}]
