8060f9da41d50cba3f31fa633e2709dc3d4e1b14
David Allsopp <david.allsopp@metastack.com>
2026-02-10 13:54:11 +0000

    Break dependency between dynlink and Ident
    Compilation_unit.to_global_ident_for_bytecode gets completely removed,
    but that's not mandatory.

--- debugger4/printval.ml
+++ debugger4/printval.ml
@@ -62,7 +62,8 @@ module EvalPath =
         | None -> raise Error
 
     let rec eval_address = function
-    | Env.Aunit cu -> eval_id (cu |> Compilation_unit.to_global_ident_for_bytecode)
+    | Env.Aunit cu ->
+        eval_id (Ident.create_persistent (Compilation_unit.full_path_as_string cu))
     | Env.Alocal id -> eval_id id
     | Env.Adot(root, _field_sorts, pos) ->
         (* We can ignore [_field_sorts] since the debugger runs only bytecode *)
